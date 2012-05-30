504b4b9973cc8525cf31049990f436c2bde67efb
Bart Meulenbroeks <->
2012-05-30 19:48:53 +0200

    Added empty git flow plugin for git extensions.

---
+++ Plugins/GitFlow/GitFlowPlugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GitUIPluginInterfaces;
+
+namespace GitFlow
+{
+    public class GitFlowPlugin : IGitPluginForRepository
+    {
+        public string Description
+        {
+            get { return "Git flow"; }
+        }
+
+        public IGitPluginSettingsContainer Settings { get; set; }
+
+        public void Register(IGitUICommands gitUiCommands)
+        {
+            //Settings.AddSetting("Find large files bigger than (Mb)", "1");
+        }
+
+        public bool Execute(GitUIBaseEventArgs gitUiCommands)
+        {
+            return true;
+        }
+    }
+}
